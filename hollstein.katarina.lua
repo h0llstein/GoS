c00b20083987d4e55312d74de0e9406b875e6478
h0llstein <niklashollstein00@googlemail.com>
2017-03-16 00:01:43 +0100

    deddd

--- hollstein.katarina.lua
+++ hollstein.katarina.lua
@@ -43,8 +43,9 @@ function Katarina:Menu()
     self.Katarina.Combo:Boolean("E", "use E", true)
     self.Katarina.Combo:Boolean("R", "use R", true)
     self.Katarina.Combo:Boolean("RK", "Only use R if killable", false)
-    self.Katarina.Combo:Boolean("RC", "Don't cancel R", true)
+    self.Katarina.Combo:Boolean("RC", "Don't cancel R", false)
     self.Katarina.Combo:Boolean("Kill", "Only Combo if killable", false)
+
     self.Katarina:SubMenu("Harass", "Harass Settings")
     self.Katarina.Harass:Boolean("Q", "Use Q", true)
 
@@ -68,6 +69,9 @@ function Katarina:Menu()
     self.Katarina.Draw:Boolean("E", "Draw E", true)
     self.Katarina.Draw:Boolean("R", "Draw R", true)
     self.Katarina.Draw:Boolean("AA", "Draw AutoAttack", true)
+    self.Katarina.Draw:Boolean("Kill", "Draw Killable", true)
+    self.Katarina.Draw:Boolean("Dmg", "Draw Damage", true)
+
     -- body
 end
 
@@ -78,21 +82,22 @@ local resetAble = { }
 local animationCancel = { }
 local kataCounter = 0
 local killablewithR = false
+local passiveDMG = CalcDamage(myHero, target, 0,(( { [1] = 75, [2] = 80, [3] = 87, [4] = 94, [5] = 102, [6] = 111, [7] = 120, [8] = 131, [9] = 143, [10] = 155, [11] = 168, [12] = 183, [13] = 198, [14] = 214, [15] = 231, [16] = 248, [17] = 267, [18] = 287 })[GetLevel(myHero)] + GetBonusAP(myHero) *( { [1] = 0.55, [2] = 0.55, [3] = 0.55, [4] = 0.55, [5] = 0.55, [6] = 0.7, [7] = 0.7, [8] = 0.7, [9] = 0.7, [10] = 0.7, [11] = 0.85, [12] = 0.85, [13] = 0.85, [14] = 0.85, [15] = 0.85, [16] = 1, [17] = 1, [18] = 1 })[GetLevel(myHero)] + GetBonusAD(myHero) *1)))
 
 OnProcessSpell( function(unit, spell)
-    if unit == myHero and spell.name == "KatarinaR" then
+    if unit == myHero and spell.name == "KatarinaR" and self.Katarina.Combo.RC:Value() then
         inUlt = true
     end
 end )
 
 OnUpdateBuff( function(unit, buff)
-    if unit == myHero and buff.Name == "katarinasound" then
+    if unit == myHero and buff.Name == "katarinasound" and self.Katarina.Combo.RC:Value() then
         inUlt = true
     end
 end )
 
 onRemoveBuff( function(unit, buff)
-    if unit == myHero and buff.Name == "katarinasound" then
+    if unit == myHero and buff.Name == "katarinasound" and self.Katarina.Combo.RC:Value() then
         inUlt = false
     end
 end )
@@ -162,6 +167,9 @@ function Katarina:Draw()
     if self.Katarina.Draw.R:Value() then
         DrawCircle(myHero, 550, 0, 150, GoS.White)
     end
+    if self.Katarina.Draw.Kill:Value() then
+        DrawText(KatarinaKillable(target), 5, )
+    end
 end
 
 KatarinaKillable( function(target)
@@ -194,12 +202,15 @@ function Katarina:Tick()
                 castTargetSpell(target, _E)
                 if self.Katarina.Combo.W:Value() and Ready(_W) then
                     CastSpell(_W)
-                    if self.Katarina.Combo.R:Value() and Ready(_R) and self.Katarina.Combo.RK:Value() then
+                    if self.Katarina.Combo.R:Value() and Ready(_R) and self.Katarina.Combo.RK:Value() and KatarinaKillable(target) == "Killable with R" then
                         CastSpell(_R)
-                        if self.Katarina.Combo.Q:Value() and Ready(_Q) then
-                            CastSpell(_Q)
-                            if self.Katarina.Combo.E:Value() and Ready(_E) then
-                                castTargetSpell(target, _E)
+                        if inUlt == true and GetDistance(myHero, target) >= 580 then
+                            inUlt = false
+                            if self.Katarina.Combo.Q:Value() and Ready(_Q) then
+                                CastSpell(_Q)
+                                if self.Katarina.Combo.E:Value() and Ready(_E) then
+                                    castTargetSpell(target, _E)
+                                end
                             end
                         end
                     end
@@ -211,17 +222,21 @@ function Katarina:Tick()
                 castTargetSpell(target, _E)
                 if self.Katarina.Combo.W:Value() and Ready(_W) then
                     CastSpell(_W)
-                    if self.Katarina.Combo.R:Value() and Ready(_R) and self.Katarina.Combo.RK:Value() then
+                    if self.Katarina.Combo.R:Value() and Ready(_R) then
                         CastSpell(_R)
-                        if self.Katarina.Combo.Q:Value() and Ready(_Q) then
-                            CastSpell(_Q)
-                            if self.Katarina.Combo.E:Value() and Ready(_E) then
-                                castTargetSpell(target, _E)
+                        if inUlt == true and GetDistance(myHero, target) >= 580 then
+                            inUlt = false
+                            if self.Katarina.Combo.Q:Value() and Ready(_Q) then
+                                CastSpell(_Q)
+                                if self.Katarina.Combo.E:Value() and Ready(_E) then
+                                    castTargetSpell(target, _E)
+                                end
                             end
                         end
                     end
                 end
             end
         end
+        if self.Katarina.Combo.
     end
 end
